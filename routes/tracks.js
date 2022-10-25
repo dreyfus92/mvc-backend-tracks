bffa4e57cd3788d1b16b87195a2803913ffcaf5d
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-10-24 22:40:25 -0500

    added validator for tracks model and created handleValidation function in helpers

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { getItems, getItem, createItem } = require("../controllers/tracks");
+const { validatorCreateItem } = require("../validators/tracks");
 
 //TODO http://localhost/tracks GET, POST, DELETE, PUT
 
 router.get("/", getItems);
-router.post("/", createItem);
+router.post("/", validatorCreateItem, createItem);
 
 module.exports = router;
