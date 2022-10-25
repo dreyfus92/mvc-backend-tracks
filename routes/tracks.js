1bbaaaee2e4d7d71bb90b2f2181ec917152205cc
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-10-24 23:16:11 -0500

    added first middleware

--- routes/tracks.js
+++ routes/tracks.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { getItems, getItem, createItem } = require("../controllers/tracks");
 const { validatorCreateItem } = require("../validators/tracks");
+const { customHeader } = require("../middlewares/customHeader");
 
 //TODO http://localhost/tracks GET, POST, DELETE, PUT
 
 router.get("/", getItems);
-router.post("/", validatorCreateItem, createItem);
+router.post("/", validatorCreateItem, customHeader, createItem);
 
 module.exports = router;
